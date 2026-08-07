14a6ccf06e368b7f55ef73ad3fe1a6f6f4ee0697
Svetlana <brigantina@mail.ru>
2026-08-07 20:40:42 +0300

    chore: add eslint flat config with typescript-eslint and unicorn   (Fri Aug 2026-08-07 20:40:34)

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      unicorn,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...unicorn.configs.recommended.rules,
+    },
+  },
+];
