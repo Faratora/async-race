59852b1f10cb9cbd9f8c7d4a106bd4e7c79ef170
Svetlana <brigantina@mail.ru>
2026-08-28 22:13:53 +0300

    fix: path fixed   (Fri Aug 2026-08-28 22:13:37)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 
 export default defineConfig({
-  base: '/faratora-JSSB2026Q2/async-race/',
+  base: '/async-race/',
   build: {
     outDir: 'dist',
     rollupOptions: {
